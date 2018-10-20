03f0e7aecd443fb2d8badde5cd9c48c05d8b1976
ruanxinyu <1096421257@qq.com>
2018-10-20 17:14:12 +0800

    Site updated: 2018-10-20 17:13:58

--- js/main.js
+++ js/main.js
@@ -46,6 +46,39 @@ $(function () {
 
     if (document.body.scrollWidth < 768) {
         $(".col-toc").hide();
-        $(".col-content").removeClass("offset-2 col-10").addClass("col-12");
+        $(".col-content").removeClass("col-10 col-8").addClass("col-12");
+    } else {
+        var project = window.location.href.match(/\/posts\/(.*)\/\?project=([^&]*)/);
+        if (project) {
+            var projectName = decodeURI(project[2]);
+            var postName = decodeURI(project[1]);
+            $.get("/data/projects.json", function(data) {
+                for (var index in data) {
+                    if (data[index].title == projectName) {
+                        var toc = '<div class="col-2 pt-3 pl-0 position-fixed" style="overflow-y:auto; height:100%; width:100%"><h6 class="text-success">【项目】' + data[index].title  + '</h6><hr class="mt-0"><ol class="toc">';
+                        for (var index1 in data[index].posts) {
+                            if (data[index].posts[index1].url == "" || data[index].posts[index1].url == "#") {
+                                toc += '<li class="toc-item toc-level-1">' + data[index].posts[index1].title + '</li>';
+                            } else {
+                                var text = '<li class="toc-item toc-level-1"><a href="' + data[index].posts[index1].url + '?project=' + data[index].title + '">&nbsp' + data[index].posts[index1].title + '</a></li>';
+                                toc += data[index].posts[index1].title == postName ? "<strong>" + text + "</strong>" : text;
+                            }
+                            
+                            if ("subPosts" in data[index].posts[index1] && data[index].posts[index1].subPosts.length > 0) {
+                                toc += '<ol class="toc-child">';
+                                for (var index2 in data[index].posts[index1].subPosts) {
+                                    var text = '<li class="toc-item toc-level-1"><a href="' + data[index].posts[index1].subPosts[index2].url + '?project=' + data[index].title + '">&nbsp' + data[index].posts[index1].subPosts[index2].title + '</a></li>';
+                                    toc += data[index].posts[index1].subPosts[index2].title == postName ? "<strong>" + text + "</strong>" : text;
+                                }
+                                toc += '</ol>';
+                            }
+                        }
+                        toc += '</ol></div></div>';
+                        $(".col-content").removeClass("col-10 col-12").addClass("offset-2 col-8").before(toc);
+                        break;
+                    }
+                }
+            });
+        }
     }
 })
